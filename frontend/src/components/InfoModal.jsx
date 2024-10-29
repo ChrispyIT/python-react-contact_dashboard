3ce070b1fcbdf2206fbe219d503335222332594b
JChrispy <jchrispy@yahoo.com>
2024-10-29 01:02:24 +0100

    added info modal re-styled navbar

---
+++ frontend/src/components/InfoModal.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    Button,
+    Text
+  } from '@chakra-ui/react'
+  import { BsFillQuestionSquareFill } from "react-icons/bs";
+
+export const InfoModal = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+        <>
+          <Button  onClick={onOpen}>
+          <BsFillQuestionSquareFill size={20}/>
+          </Button>
+    
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Modal Title</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+              <Text>Lorem ipsum dolor sit amet, 
+                consetetur sadipscing elitr, 
+                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
+                sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</Text>
+              </ModalBody>
+    
+              <ModalFooter>
+                <Button colorScheme='blue' mr={3} onClick={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </>
+      )
+}
